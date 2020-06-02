6b0a686167875a9410ef7fd0a46d479d79bc3e8b
Shehbab-Kakkar <shehbab1@gmail.com>
2020-06-02 16:49:30 +0530

    Update main.tf

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -2,5 +2,10 @@ provider "aws" {
    region = "us-west-2"
 }
 resource "aws_vpc" "module_vpc" {
-  cidr_block = ""
+  cidr_block = var.vpc_cidr_block
 }
+resource "aws_subnet" "public_subnet" {
+  cidr_block = ""
+  vpc_id = "$aws_vpc.module_vpc.id"
+}
+
